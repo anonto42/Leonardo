bc616b0c43d2202a657de2305430d75ec049ff54
Sohidul Islam Ananto <anontom90@gmail.com>
2025-09-07 10:20:40 +0600

    bollmq setup complite

--- src/bollmq.worker.ts
+++ src/bollmq.worker.ts
@@ -1,17 +1,24 @@
 import { Worker } from "bullmq";
 import { redisConnection } from "./app/modules/Queue/redis";
+import mongoose from "mongoose";
+import config from "./config";
 import workerFunction from "./app/modules/Queue/function";
 
+;( async () => {
+    const db = await mongoose.connect(`mongodb://localhost:${config.database_port}/${config.database_name}`);
+    console.log("DB Connected on the BollMQ worker on:--> ",db.connection.host)
+})()
+
 const worker = new Worker(
-    "task-queue",
-    workerFunction,
-    { connection: redisConnection }
+  "task-queue",
+  workerFunction,
+  { connection: redisConnection }
 );
 
-worker.on("completed", ( job ) => {
-    console.log(`🎉 Job ${job.id} finished`)
+worker.on("completed", (job) => {
+  console.log(`🎉 Job ${job.id} finished`);
 });
 
-worker.on("failed", ( job, err) => {
-    console.log(`❌ Job ${job?.id} failed: ${err.message}`)
+worker.on("failed", (job, err) => {
+  console.log(`❌ Job ${job?.id} failed: ${err.message}`);
 });
