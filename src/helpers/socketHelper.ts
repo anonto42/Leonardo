3dbc8a94200c669df7b99e0f85692db149beb71a
Sohidul Islam Ananto <anontom90@gmail.com>
2025-09-09 12:14:42 +0600

    events created for the socket helper

--- src/helpers/socketHelper.ts
+++ src/helpers/socketHelper.ts
@@ -1,10 +1,28 @@
 import colors from 'colors';
 import { Server } from 'socket.io';
 import { logger } from '../shared/logger';
+import { RedisDB } from '../app/modules/Queue/redis';
 
 const socket = (io: Server) => {
   io.on('connection', socket => {
     logger.info(colors.blue('A user connected'));
+
+    //Register the user with the socket id
+    socket.on("register", async ( userID: string) => {
+      await RedisDB.set(`user:${userID}`,socket.id);
+      logger.info( colors.yellow(`${userID} was added on socket: ${socket.id}`) );
+    });
+
+    // Send data to user
+    socket.on("resend", ( data ) => {
+      
+      socket.to(data.data.socketId).emit("notification", {
+        type: data.type,
+        message: data.message,
+        completedAt: data.data.completedAt
+      })
+
+    })
 
     //disconnect
     socket.on('disconnect', () => {
